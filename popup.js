cccc6fdf181c2547234e346c98f5a69a652ace98
Lucas <lbozz4@gmail.com>
2019-10-12 15:25:58 -0400

    IT WORKS
    uhh... made it work.

--- popup.js
+++ popup.js
@@ -1,12 +1,52 @@
-var MFGPN = "SMA10F";
 
-let download = document.getElementById('download');
+var download = document.getElementById('download');
+
+
+
+var VENDOR_PN = "NA"
+var MANUFACTURER = "NA"
+var PARTNAME = "NA"
+var PARTDESC = "NA"
+var EXDESC = "NA"
 
 download.onclick = function (element) {
-    var blob = new Blob(["< note ><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note >"], { type: "text/xml" });
-    var url = URL.createObjectURL(blob);
-    chrome.downloads.download({
-        url: url, // The object URL can be used as download URL
-        filename: MFGPN+".xml"
+    // Inject scrape.js and scrape dat der good part information
+    chrome.tabs.executeScript({
+        file: 'scrape.js'
     });
 };
+
+// Recieve the scraped variables as a message and then assemble the XML and download
+chrome.runtime.onMessage.addListener(
+    function (partinfo, sender, sendResponse) {
+        VENDOR_PN = partinfo.VENDOR_PN;
+        MANUFACTURER = partinfo.MANUFACTURER;
+        PARTNAME = partinfo.PARTNAME;
+        PARTDESC = partinfo.PARTDESC;
+
+        // Generate the CADInt XML file that can be imported into a new part
+        var xmlblob =
+            '<?xml version="1.0" encoding="ISO-8859-1"?>' +
+            "<PartNumber Name=\"" + PARTNAME + "\" Ver=\"1\">" +
+            "<Properties Ver=\"1\">" +
+            "<A Key=\"PARTNAME\" Value=\"" + PARTNAME + "\"/>" +
+            "<A Key=\"ALTPARTREF\" Value=\"\"/>" +
+            "<A Key=\"PARTDESC\" Value=\"" + PARTDESC + "\"/>" +
+            "<A Key=\"MANUFACTURER\" Value=\"" + MANUFACTURER + "\"/>" +
+            "<A Key=\"PREFIX\" Value=\"\"/>" +
+            "<A Key=\"TOLERANCE\" Value=\"\"/>" +
+            "<A Key=\"VALUE\" Value=\"\"/>" +
+            "<A Key=\"VENDOR\" Value=\"Digi-Key\"/>" +
+            "<A Key=\"VENDOR_PN\" Value=\"" + VENDOR_PN + "\"/>" +
+            "</Properties>" +
+            "</PartNumber>";
+
+        var blob = new Blob([xmlblob], { type: "text/xml" });
+        var url = URL.createObjectURL(blob);
+        chrome.downloads.download({
+            url: url, // The object URL can be used as download URL
+            filename: PARTNAME + ".xml"
+        });
+
+    }
+);
--- scrape.js
+++ scrape.js
@@ -1,11 +1,11 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
 
 var tds = $("#product-overview").find("td");
-var DKPN = $(tds[0]).text().trim();
-var MFG = $(tds[2]).text().trim();
-var MFGPN = $(tds[4]).text().trim();
-var DESC = $(tds[6]).text().trim();
+var VENDOR_PN = $(tds[0]).text().trim();
+var MANUFACTURER = $(tds[2]).text().trim();
+var PARTNAME = $(tds[4]).text().trim();
+var PARTDESC = $(tds[6]).text().trim();
 var EXDESC = $(tds[10]).text().trim();
 
-alert("hello!");
+//send these variables back up to popup.js
+chrome.runtime.sendMessage({VENDOR_PN:VENDOR_PN, MANUFACTURER:MANUFACTURER, PARTNAME:PARTNAME, PARTDESC:PARTDESC});
 
